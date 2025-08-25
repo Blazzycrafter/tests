1bcf6a6f34f19884220b3fa1fd9eaf92343306eb
Blazzycrafter <39300111+Blazzycrafter@users.noreply.github.com>
2025-08-25 23:44:21 +0200

    Add files via upload

---
+++ setup.ps1
@@ -0,0 +1,7 @@
+﻿# Sysprep starten
+Start-Process -FilePath ".\oobe\windeploy" -Wait
+
+# Warten bis Sysprep-Prozess beendet ist
+Wait-Process -Name "windeploy" -ErrorAction SilentlyContinue
+
+Read-Host -Prompt (Waiting for ENTER)
