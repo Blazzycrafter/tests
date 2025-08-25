b2aa4157202038fd4677fa82af66f129394437ac
Blazzycrafter <39300111+Blazzycrafter@users.noreply.github.com>
2025-08-25 23:00:44 +0200

    Update tosysprep.ps1

--- tosysprep.ps1
+++ tosysprep.ps1
@@ -2,7 +2,11 @@
 
 #Invoke-WebRequest -Uri $dlp -OutFile "C:\setup\setup.ps1"
 
-.\sysprep\sysprep.exe /generalize /oobe /quit
+# Sysprep starten
+Start-Process -FilePath ".\sysprep\sysprep.exe" -ArgumentList "/generalize /oobe /quit" -Wait
+
+# Warten bis Sysprep-Prozess beendet ist
+Wait-Process -Name "sysprep" -ErrorAction SilentlyContinue
 
 # Den Registry-Wert ändern
 Set-ItemProperty -Path "HKLM:\System\Setup" -Name "CmdLine" -Value "cmd.exe"
