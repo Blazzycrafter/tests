608c30010ffa25635af13c33fc7f8c31a83aa713
Blazzycrafter <39300111+Blazzycrafter@users.noreply.github.com>
2025-08-26 00:02:26 +0200

    Update setup.ps1

--- setup.ps1
+++ setup.ps1
@@ -1,7 +1,29 @@
-﻿# Sysprep starten
+# hier ist setup.ps1
+
+# windeploy starten
 Start-Process -FilePath ".\oobe\windeploy" -Wait
 
 # Warten bis Sysprep-Prozess beendet ist
 Wait-Process -Name "windeploy" -ErrorAction SilentlyContinue
 
-Read-Host -Prompt (Waiting for ENTER)
+# Snapshot aller aktuell existierenden User in eine Variable
+$userSnapshot = Get-LocalUser | Select-Object -ExpandProperty Name
+
+# Snapshot aller aktuell laufenden Prozesse in eine Variable
+$processSnapshot = Get-Process | Select-Object -ExpandProperty ProcessName
+
+# ms-cxh:localonly starten
+Start-Process -FilePath "ms-cxh:localonly"
+
+# Neuen Snapshot aller Prozesse
+$newProcessSnapshot = Get-Process | Select-Object -ExpandProperty ProcessName
+
+# Den neuen Prozess ermitteln
+$newProcess = $newProcessSnapshot | Where-Object { $_ -notin $processSnapshot }
+
+Write-Output "Neuer Prozess gestartet: $newProcess"
+
+# Auf Beendigung des neuen Prozesses warten
+if ($newProcess) {
+    Wait-Process -Name $newProcess -ErrorAction SilentlyContinue
+}
